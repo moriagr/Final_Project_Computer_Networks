929aea88f8c21af15a3518140f227161df25e709
moriagr <moriagro@gmail.com>
2023-08-10 14:38:15 +0300

    adding res directory

--- src/show_single_graph.py
+++ src/show_single_graph.py
@@ -3,8 +3,13 @@ import matplotlib.pyplot as plt
 
 #  This function present graph just like the graph in the article, fig. 8
 def present_graph(timestamps, packet_lengths):
-    plt.bar(timestamps, packet_lengths, width=0.1, align='center', color='black')
+    plt.bar(timestamps, packet_lengths, width=0.1, align='edge', color='black')
+    # plt.ylim(0, 1500)
     plt.xlabel('Time (seconds)')
     plt.ylabel('Packet Length (MTU-sized chunks)')
-    plt.title('Packet Length over Time (Scatter Plot)')
+    plt.title('Packet Length over Time (Bar Plot)')
+    # Save the plot to the "res" directory
+    plt.savefig("../res/example_plot.png")
+
     plt.show()
+
